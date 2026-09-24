8fa425e934f32b741a80f4ae60628c14c75a782e
adityachoudhary24 <23wj1a6703@gniindia.org>
2026-09-24 21:45:41 +0530

    Time: 0 ms (100%), Space: 13.3 MB (76.89%) - LeetHub

--- 0412-fizz-buzz/0412-fizz-buzz.py
+++ 0412-fizz-buzz/0412-fizz-buzz.py
@@ -1,11 +1,8 @@
 class Solution(object):
     def fizzBuzz(self, n):
-        """
-        :type n: int
-        :rtype: List[str]
-        """
         ans=[]
         for i in range(1,n+1):
+
             if i%3==0 and i%5==0:
                 ans.append("FizzBuzz")
             elif i%3==0:
@@ -14,4 +11,8 @@ class Solution(object):
                 ans.append("Buzz")
             else:
                 ans.append(str(i))
-        return ans                   
+        return ans                 
+
+
+        
+    
